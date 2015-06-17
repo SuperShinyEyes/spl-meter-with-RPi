c6990244e86eba574b669d45659776a159d07dc2
Young Park <i.make.seyoung@gmail.com>
2015-06-17 15:20:08 +0900

    TKinter canvas testing

--- tkinter_test.py
+++ tkinter_test.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python
+
+from Tkinter import *
+
+def onclick():
+   pass
+
+root = Tk()
+text = Text(root)
+text.insert(INSERT, "Hello.....")
+text.insert(END, "Bye Bye.....")
+text.pack()
+
+text.tag_add("here", "1.0", "1.4")
+text.tag_add("start", "1.8", "1.13")
+text.tag_config("here", background="yellow", foreground="blue")
+text.tag_config("start", background="black", foreground="green")
+root.mainloop()
