03fa1a8cf0e10d144f6541f99409e44d88dfa34d
Young Park <i.make.seyoung@gmail.com>
2015-06-17 15:13:45 +0900

    Working on concurrent programming with Firefox

--- spl3.py
+++ spl3.py
@@ -20,9 +20,23 @@ CHANNEL = 1    # 1 means mono. If stereo, put 2
 #RATE = 44300   # Logitech HD 720p has rate 48000Hz
 RATE = 48000
 
-#error_count = 0
-
 numerator, denominator = spl.A_weighting(RATE)
+
+'''
+RPi 2 has a limited cpu power compared to laptops.
+Refreshing Firefox demands long time that pyaudio stream get overflowed.
+(IOError - Buffer overflow)
+Therefore we use multi threads and FIFO Queue to solve this issue.
+For Queue read:
+https://docs.python.org/2/library/queue.html
+For Thread read:
+
+'''
+from threading import Thread
+from Queue import Full, Queue
+chunk_q = Queue(maxsize=3)
+decibel_q = Queue(maxsize=3)
+
 
 '''
 Listen to mic
@@ -45,7 +59,6 @@ single_decibel_file_path = '/home/pi/spl-meter-with-RPi/single_decibel.txt'
 def is_meaningful(old, new):
     return abs(old - new) > 3
 
-
 def make_sure_path_exists(path):
 	try:
 		os.makedirs(path)
@@ -67,6 +80,34 @@ def refresh():
 
 print "Listening"
 
+def add_decibel_queue(decibel):
+    try:
+        decibel_q.put_nowait(new_decibel)
+    except Full, e:
+        print("decibel queue is full!")
+    
+
+def get_decibel(old=0):
+    while True:
+        if not chunk_q.empty():
+            block = chunk_q.get()
+            
+            ## Int16 is a numpy data type which is Integer (-32768 to 32767)
+            ## If you put Int8 or Int32, the result numbers will be ridiculous
+            decoded_block = numpy.fromstring(block, 'Int16')
+
+            ## This is where you apply A-weighted filter
+            y = lfilter(numerator, denominator, decoded_block)
+            new_decibel = 20*numpy.log10(spl.rms_flat(y))
+            
+            if is_meaningful(old, new_decibel):
+                old = new_decibel
+                print('A-weighted: {:+.2f} dB'.format(new_decibel))
+                update_decibel_text(new_decibel)
+                add_decibel_queue(new_decibel)
+
+    
+
 def listen(old=0, error_count=0):
     while True:
         try:
@@ -76,17 +117,6 @@ def listen(old=0, error_count=0):
             error_count += 1
             print(" (%d) Error recording: %s" % (error_count, e))
         else:
-            ## Int16 is a numpy data type which is Integer (-32768 to 32767)
-            ## If you put Int8 or Int32, the result numbers will be ridiculous
-            decoded_block = numpy.fromstring(block, 'Int16')
-            ## This is where you apply A-weighted filter
-            y = lfilter(numerator, denominator, decoded_block)
-            new_decibel = 20*numpy.log10(spl.rms_flat(y))
-            if is_meaningful(old, new_decibel):
-                old = new_decibel
-                print('A-weighted: {:+.2f} dB'.format(new_decibel))
-                update_decibel_text(new_decibel)
-                refresh()
 
 
     stream.stop_stream()
@@ -97,5 +127,8 @@ def listen(old=0, error_count=0):
 
 if __name__ == '__main__':
     driver = webdriver.Firefox()
+    chunk_thread = Thread(target=listen)
+    chunk_thread.daemon = True
+    decibel_thread = Thread(target=get_decibel)
     listen()
     driver.close()
